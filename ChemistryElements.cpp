2bc65b3d2402fb19db8bef6ffe72100da317740b
Forrest Baird <forrestmsbaird@gmail.com>
2022-05-11 09:15:42 -0700

    Element Objects

--- ChemistryElements.cpp
+++ ChemistryElements.cpp
@@ -29,8 +29,8 @@ class AtomicElement {
         // Atomic Properties (See Wikipedia)
 
         // Constructor
-        AtomicElement(string sConAtomElName, string sConAtomElLet, string sConElType, 
-            float iConAtMass, int iConAtNum, string sConElConfig, string sConPerTblBlock, float iConElNeg, int iConValEl){
+        AtomicElement(string sConAtomElName, string sConAtomElLet, int iConAtNum, float iConAtMass,  
+              string sConElType, string sConElConfig, string sConPerTblBlock, float iConElNeg, int iConValEl){
 
                 sAtomicElementName = sConAtomElName;
                 sAtomicElementLetters = sConAtmElLet;
@@ -53,26 +53,66 @@ class AtomicElement {
 void LoadChemistryElements(){
 
   //AtomicElement Element("Element Name", "Element Symbol", "Element Type", Atomic Mass, Atomic No, "Electron Config", "Periodic Table Block", Electronegativity, Valence Electrons)
-    AtomicElement Hydrogen("Hydrogen", "H", "nonemetal", 1.00797, 1,);
-    AtomicElement Helium("Helium", "He");
+    AtomicElement Hydrogen("Hydrogen", "H", 1);
+    AtomicElement Helium("Helium", "He", 2);
     
-    AtomicElement Lithium("Lithium", "Li");
-    AtomicElement Beryllium("Beryllium", "Be");
-    AtomicElement Boron("Boron", "B");
-    AtomicElement Carbon("Carbon", "C");
-    AtomicElement Nitrogen();
-    AtomicElement Oxygen();
-    AtomicElement Fluorine();
-    AtomicElement Neon();
+    AtomicElement Lithium("Lithium", "Li", 3);
+    AtomicElement Beryllium("Beryllium", "Be", 4);
+    AtomicElement Boron("Boron", "B", 5);
+    AtomicElement Carbon("Carbon", "C", 6);
+    AtomicElement Nitrogen("Nitrogen", "N", 7);
+    AtomicElement Oxygen("Oxygen", "O", 8);
+    AtomicElement Fluorine("Fluorine", "F", 9);
+    AtomicElement Neon("Neon", "N", 10);
 
-    AtomicElement ;
-    AtomicElement ;
-    AtomicElement ;
-    AtomicElement ;
-    AtomicElement ;
-    AtomicElement ;
-    AtomicElement ;
-    AtomicElement ;
+    AtomicElement Sodium("Sodium", );
+    AtomicElement Magnesium();
+    AtomicElement Aluminum();
+    AtomicElement Silicon();
+    AtomicElement Phosphorus();
+    AtomicElement Sulfur();
+    AtomicElement Chlorine();
+    AtomicElement Argon("Argon", "Ar", 18);
+
+    AtomicElement Potassium();
+    AtomicElement Calcium();
+    AtomicElement Scandium();
+    AtomicElement Titanium();
+    AtomicElement Vanadium();
+    AtomicElement Chromium();
+    AtomicElement Manganese();
+    AtomicElement Iron();
+    AtomicElement Cobalt();
+    AtomicElement Nickel();
+    AtomicElement Copper();
+    AtomicElement Zinc();
+    AtomicElement Gallium();
+    AtomicElement Germanium();
+    AtomicElement Arsenic();
+    AtomicElement Selenium();
+    AtomicElement Bromine();
+    AtomicElement Krypton();
+
+    AtomicElement Rubidium();
+    AtomicElement Strontium();
+    AtomicElement Yttrium();
+    AtomicElement Zirconium();
+    AtomicElement Niobium();
+    AtomicElement Molybdenum();
+    AtomicElement Technetium();
+    AtomicElement Ruthenium();
+    AtomicElement Rhodium();
+    AtomicElement Palladium();
+    AtomicElement Silver();
+    AtomicElement Cadmium();
+    AtomicElement Indium();
+    AtomicElement Tin();
+    AtomicElement Antimony();
+    AtomicElement Tellurium();
+    AtomicElement Iodine("Iodine", "I", 53);
+    AtomicElement Xeon("Xeon", "Xe", 54);
+
+    // AtomicElement ();
 
 
 }
