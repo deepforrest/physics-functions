c28bdd6e3a4d895ac436048b135ba3d509919d34
Forrest Baird <forrestmsbaird@gmail.com>
2022-05-12 19:33:00 -0700

    Linear Equations, Combinatorics, and more!

--- FunctionsElementary.cpp
+++ FunctionsElementary.cpp
@@ -500,3 +500,94 @@ string iConvertToFraction(double iNum, double iDenom) {
 
     return "Function not ready."
 }
+
+int iFactorial (int iNumInput) {
+
+    iFactResult = 1;
+
+    for (int iNum = iInput; iNum > 0; iNum--) {
+
+        iFactResult *= iNum;
+
+    }
+
+    return iFactResult;
+
+}
+
+int iCalculateCombinations (int iTotalItems, int iNumOfItemsSelected) {
+
+    int iCalculatedCombNum = iFactorial(iTotalItems);
+    int iCalculatedCombDen = iFactorial(iTotalItems - iNumOfItemsSelected) * iFactorial(iNumOfItemsSelected);
+
+    return iCalculatedCombNum / iCalculateddCombDen;
+
+}
+
+int iCalculatePermutations (int iTotalItems, int iNumOfItemsSelected) {
+
+    int iCalculatedCombNum = iFactorial(iTotalItems);
+    int iCalculatedCombDen = iFactorial(iTotalItems - iNumOfItemsSelected);
+
+    return iCalculatedCombNum / iCalculateddCombDen;
+
+}
+
+double iQuadSolPos (double iA, double iB, double iC) {
+
+    return (- iB + pow(pow(iB, 2) - 4 * iA * iC, 1/2)) / (2 * iA);
+
+}
+
+double iQuadSolNeg (double iA, double iB, double iC) {
+
+    return (- iB - pow(pow(iB, 2) - 4 * iA * iC, 1/2)) / (2 * iA);
+
+}
+
+double iArrQuadraticSolution[2] (double iA, double iB, double iC){
+
+    iDiscr = pow(iB, 2) - 4 * iA * iC;
+
+    if iDiscr < 0 {
+
+
+
+    }
+
+    return {iArrQuadSolPos(iA, iB, iC), iArrQuadSolNeg(iA, iB, iC)};
+
+}
+
+double iLinearEqX (double iY, double iVert, double iSlope) { 
+
+    // Validations
+    if (iSlope = 0) {
+
+        cout << "Invalid slope!" << lend;
+        exit(0);
+    }
+
+    return (iY - iVert)/iSlope;
+
+}
+
+double iLinearEqY (double iSlope, double iX, double iVert) {
+
+    return iSlope * iX + iVert;
+
+}
+
+double iLinearXInt (double iVert, double iSlope) {
+
+    // Let iY = 0
+    return iLinearEqX(0, iVert, iSlope);
+
+}
+
+double iLinearYInt (double iVert) {
+
+    // Let iX = 0
+    return iLinearEqY(0, 0, iVert);
+
+}
